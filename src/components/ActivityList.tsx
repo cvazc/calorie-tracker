e8e1e86895d6a7642a655111b10b1f89cd5cc345
secontreras <se.contreras@mavi.mx>
2024-11-26 14:28:22 -0600

    add delete functionality to ActivityList and update activityReducer to handle deletion

--- src/components/ActivityList.tsx
+++ src/components/ActivityList.tsx
@@ -1,7 +1,7 @@
 import { useMemo, Dispatch } from "react";
 import { Activity } from "../types";
 import { categories } from "../data/categories";
-import { PencilSquareIcon } from "@heroicons/react/24/outline";
+import { PencilSquareIcon, XCircleIcon } from "@heroicons/react/24/outline";
 import { ActivityActions } from "../reducers/activityReducer";
 
 type ActivityListProps = {
@@ -18,13 +18,18 @@ export default function ActivityList({
             categories.map((cat) => (cat.id === category ? cat.name : "")),
         [activities]
     );
+
+    const isEmptyActivities = useMemo(() => activities.length === 0, [activities])
+
     return (
         <>
             <h2 className="text-4xl font-bold text-slate-600 text-center">
                 Comida y Actividades
             </h2>
 
-            {activities.map((activity) => (
+            {isEmptyActivities ? <p className="text-center py-5">No hay actividades</p> : 
+
+            activities.map((activity) => (
                 <div
                     key={activity.id}
                     className="px-5 py-10 bg-white mt-5 flex justify-between"
@@ -57,6 +62,17 @@ export default function ActivityList({
                             }
                         >
                             <PencilSquareIcon className="h-8 w-8 text-gray-800" />
+                        </button>
+
+                        <button
+                            onClick={() =>
+                                dispatch({
+                                    type: "delete-activity",
+                                    payload: { id: activity.id },
+                                })
+                            }
+                        >
+                            <XCircleIcon className="h-8 w-8 text-red-500" />
                         </button>
                     </div>
                 </div>
