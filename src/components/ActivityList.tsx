3c1dde930d26eb1e3138f25eaf6d03c0e20034a7
secontreras <se.contreras@mavi.mx>
2024-11-26 15:27:15 -0600

    add localStorage integration and restart functionality to activity management

--- src/components/ActivityList.tsx
+++ src/components/ActivityList.tsx
@@ -19,7 +19,10 @@ export default function ActivityList({
         [activities]
     );
 
-    const isEmptyActivities = useMemo(() => activities.length === 0, [activities])
+    const isEmptyActivities = useMemo(
+        () => activities.length === 0,
+        [activities]
+    );
 
     return (
         <>
@@ -27,56 +30,58 @@ export default function ActivityList({
                 Comida y Actividades
             </h2>
 
-            {isEmptyActivities ? <p className="text-center py-5">No hay actividades</p> : 
-
-            activities.map((activity) => (
-                <div
-                    key={activity.id}
-                    className="px-5 py-10 bg-white mt-5 flex justify-between"
-                >
-                    <div className="space-y-2 relative">
-                        <p
-                            className={`absolute -top-8 -left-8 px-10 py-2 text-white uppercase font-bold 
+            {isEmptyActivities ? (
+                <p className="text-center py-5">No hay actividades</p>
+            ) : (
+                activities.map((activity) => (
+                    <div
+                        key={activity.id}
+                        className="px-5 py-10 bg-white mt-5 flex justify-between shadow"
+                    >
+                        <div className="space-y-2 relative">
+                            <p
+                                className={`absolute -top-8 -left-8 px-10 py-2 text-white uppercase font-bold 
                                 ${
                                     activity.category === 1
                                         ? "bg-lime-500"
                                         : "bg-orange-500"
                                 }`}
-                        >
-                            {categoryName(+activity.category)}
-                        </p>
-                        <p className="text-2xl font-bold pt-5">
-                            {activity.name}
-                        </p>
-                        <p className="text-4xl font-black text-lime-500">
-                            {activity.calories} <span>Calorias</span>
-                        </p>
-                    </div>
-                    <div className="flex gap-5 item-center">
-                        <button
-                            onClick={() =>
-                                dispatch({
-                                    type: "set-activeId",
-                                    payload: { id: activity.id },
-                                })
-                            }
-                        >
-                            <PencilSquareIcon className="h-8 w-8 text-gray-800" />
-                        </button>
+                            >
+                                {categoryName(+activity.category)}
+                            </p>
+                            <p className="text-2xl font-bold pt-5">
+                                {activity.name}
+                            </p>
+                            <p className="text-4xl font-black text-lime-500">
+                                {activity.calories} <span>Calorias</span>
+                            </p>
+                        </div>
+                        <div className="flex gap-5 item-center">
+                            <button
+                                onClick={() =>
+                                    dispatch({
+                                        type: "set-activeId",
+                                        payload: { id: activity.id },
+                                    })
+                                }
+                            >
+                                <PencilSquareIcon className="h-8 w-8 text-gray-800" />
+                            </button>
 
-                        <button
-                            onClick={() =>
-                                dispatch({
-                                    type: "delete-activity",
-                                    payload: { id: activity.id },
-                                })
-                            }
-                        >
-                            <XCircleIcon className="h-8 w-8 text-red-500" />
-                        </button>
+                            <button
+                                onClick={() =>
+                                    dispatch({
+                                        type: "delete-activity",
+                                        payload: { id: activity.id },
+                                    })
+                                }
+                            >
+                                <XCircleIcon className="h-8 w-8 text-red-500" />
+                            </button>
+                        </div>
                     </div>
-                </div>
-            ))}
+                ))
+            )}
         </>
     );
 }
--- src/reducers/activityReducer.ts
+++ src/reducers/activityReducer.ts
@@ -3,15 +3,21 @@ import { Activity } from "../types";
 export type ActivityActions =
     | { type: "save-activity"; payload: { newActivity: Activity } }
     | { type: "set-activeId"; payload: { id: Activity["id"] } }
-    | { type: "delete-activity"; payload: { id: Activity["id"] } } ;
+    | { type: "delete-activity"; payload: { id: Activity["id"] } }
+    | { type: "restart-app" };
 
 export type ActivityState = {
     activities: Activity[];
     activeId: Activity["id"];
 };
 
+const localStorageActivities = (): Activity[] => {
+    const activities = localStorage.getItem("activities");
+    return activities ? JSON.parse(activities) : [];
+};
+
 export const initialState: ActivityState = {
-    activities: [],
+    activities: localStorageActivities(),
     activeId: "",
 };
 
@@ -48,11 +54,20 @@ export const activityReducer = (
         };
     }
 
-    if (action.type === 'delete-activity') {
+    if (action.type === "delete-activity") {
         return {
             ...state,
-            activities: state.activities.filter(activity => activity.id !== action.payload.id)
-        }
+            activities: state.activities.filter(
+                (activity) => activity.id !== action.payload.id
+            ),
+        };
+    }
+
+    if (action.type === "restart-app") {
+        return {
+            activities: [],
+            activteId: "",
+        };
     }
 
     return state;
